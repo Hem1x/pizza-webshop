f6e22d614a9445b7cb7d977bdd0dfce3d95cdf2a
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-08-10 15:37:26 +0300

    setup project & componented design

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+const App = () => {
+  return (
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
